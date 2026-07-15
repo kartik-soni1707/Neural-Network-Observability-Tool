9e05d4a542dda72a40ca534d3e98a188e08622f9
kartik-soni1707 <its.kartiksoni@gmail.com>
2026-07-15 13:25:17 +0530

    sigmoid + relu

--- signmoid_relu.py
+++ signmoid_relu.py
@@ -22,3 +22,15 @@ class Solution:
         for i in range(len(z)):
             z[i]=helper(z[i])
         return z
+
+#Activation functions add non linearity
+#Use sigmoid for binary output due to vanishing gradient
+#Use relu for hidden layers does not lead to vanishing gradient 
+if __name__ == "__main__":
+    sol = Solution()
+
+    z1 = np.array([0.0, 1.0, -1.0, 5.0])
+    print(sol.sigmoid(z1))   # [0.5     0.73106 0.26894 0.99331]
+
+    z2 = np.array([-2.0, 0.0, 3.5, -0.1])
+    print(sol.relu(z2))      # [0.  0.  3.5 0. ]
