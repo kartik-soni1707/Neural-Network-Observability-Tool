d86c87fb3734b75c828a102af765be7feb371013
kartik-soni1707 <its.kartiksoni@gmail.com>
2026-07-15 13:18:42 +0530

    grad descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -9,3 +9,12 @@ class Solution:
         for i in range(iterations):
             res-=learning_rate*2*res
         return round(res,5)
+#Learning point: stepping through loss function to find minima 
+#learning_rate: single most important hyper parameter
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.get_minimizer(0, 0.1, 5))    # 5.0
+    print(sol.get_minimizer(10, 0.01, 5))  # 4.08536
+    print(sol.get_minimizer(10, 0.1, 5))   # 0.53687
+    print(sol.get_minimizer(10, 0.5, 5))   # 0.0
